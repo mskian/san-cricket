9b100ecc804f4d8767a8d70e69eb23bea424f355
Santhosh Veer <veerfun23@gmail.com>
2020-10-14 18:15:39 +0530

    Create options.php

---
+++ options.php
@@ -0,0 +1,19 @@
+<div class="wrap">
+<div id="post-body" class="metabox-holder columns-2">
+<div id="post-body-content">
+<div class="postbox">
+<div class="inside">
+<h2>Live Cricket Score</h2>
+<strong><p>Get API &rArr; <a rel="nofollow noopener noreferrer" href="" target="_blank">Click Here</a></p></strong>
+<strong><p>Live Cricket Score Plugin Shortcode &rArr; <code>[sancri]</code></p></strong>
+<form method="post" action="options.php">
+<?php settings_fields('sancric_mskc_topt'); ?>
+<p>API Key</p>
+<input type="password" name="sancric_userapi_key" value="<?php echo get_option('sancric_userapi_key'); ?>" />
+<?php submit_button();?>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
